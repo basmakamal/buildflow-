90d1c0ab0cf460091d98be83da7f4c5194c2ce1c
basma kamal <basmakamal61@gmail.com>
2026-08-15 21:37:54 +0300

    feat(database): add unit of work with transactional outbox

---
+++ packages/database/src/index.ts
@@ -0,0 +1,24 @@
+export {
+  type RequestContext,
+  runWithTenantContext,
+  runWithoutTenantScope,
+  getTenantContext,
+  getCompanyId,
+  isBypassingTenantScope,
+} from './tenant-context'
+
+export { withTenantScope, GLOBAL_MODELS, Prisma } from './tenant-extension'
+export {
+  createDatabase,
+  TenantConnectionRegistry,
+  type Database,
+  type DatabaseOptions,
+} from './client'
+export {
+  PrismaUnitOfWork,
+  OutboxRelay,
+  type UnitOfWork,
+  type Tx,
+  type OutboxPublisher,
+  type OutboxRow,
+} from './unit-of-work'
